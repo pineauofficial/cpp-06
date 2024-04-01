3fef53d35fa3d6c0fe3deadc18c7109ee4803cbd
Pierrick NEAU <pierrickn44@gmail.com>
2024-04-01 15:20:48 +0200

    main update

--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:58:55 by pineau            #+#    #+#             */
-/*   Updated: 2024/03/31 13:46:16 by pineau           ###   ########.fr       */
+/*   Updated: 2024/04/01 15:20:07 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int main()
 	data.str = "Hello";
 	data.n = 42;
 
+	std::cout << "adresse data init: " << &data << std::endl;
 	raw = Serializer::serialize(&data);
 	Serializer::deserialize(raw);
 	return 0;	
