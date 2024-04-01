780cfc4a148b2c2be4a82429615c4d98a185b0cf
Pierrick NEAU <pierrickn44@gmail.com>
2024-04-01 15:31:12 +0200

    ex02 done & updated

--- ex02/sources/Base.cpp
+++ ex02/sources/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:47:40 by pineau            #+#    #+#             */
-/*   Updated: 2024/03/11 19:11:21 by pineau           ###   ########.fr       */
+/*   Updated: 2024/04/01 15:28:59 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ Base* Base::generate(void)
 {
 	srand (time(NULL));
 	int i = rand() % 3;
-	std::cout << "i = " << i << std::endl;
 	if (i == 0)
 		return std::cout << "A generated" << std::endl, new A;
 	else if (i == 1)
@@ -44,10 +43,12 @@ void Base::identify(Base* p) {
 }
 
 void Base::identify(Base& p) {
+	int i = 0;
 	try {
 		A &a = dynamic_cast<A&>(p);
 		std::cout << "A identified" << std::endl;
 		(void)a;
+		i = 1;
 	}
 	catch (std::exception &e) {
 	}
@@ -55,6 +56,7 @@ void Base::identify(Base& p) {
 		B &b = dynamic_cast<B&>(p);
 		std::cout << "B identified" << std::endl;
 		(void)b;
+		i = 1;
 	}
 	catch (std::exception &e) {
 	}
@@ -62,7 +64,10 @@ void Base::identify(Base& p) {
 		C &c = dynamic_cast<C&>(p);
 		std::cout << "C identified" << std::endl;
 		(void)c;
+		i = 1;
 	}
 	catch (std::exception &e) {
 	}
+	if (i == 0)
+		std::cout << "Error" << std::endl;
 }
--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:48:00 by pineau            #+#    #+#             */
-/*   Updated: 2024/03/31 13:55:59 by pineau           ###   ########.fr       */
+/*   Updated: 2024/04/01 15:29:42 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,16 @@
 int main()
 {
 	Base base;
+	Base base1;
+	std::cout << "-----random Base-----" << std::endl;
 	Base* p = base.generate();
+	std::cout << "-----Base generated-----" << std::endl;
+	Base* p1 = NULL;
 	base.identify(p);
 	base.identify(*p);
+	std::cout << "-----Base NULL-----" << std::endl;
+	base1.identify(p1);
+	base1.identify(*p1);
 
 	delete p;
 	return 0;
